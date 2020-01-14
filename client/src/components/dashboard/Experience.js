c4264b59f6e2c926da349a07c6b02ebfb5c123f2
enzo <florenzodinglasan@gmail.com>
2020-01-15 05:53:39 +0800

    demo

---
+++ client/src/components/dashboard/Experience.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import Moment from "react-moment";
+import { deleteExperience } from "../../actions/profileActions";
+
+class Experience extends Component {
+  onDeleteClick(id) {
+    this.props.deleteExperience(id);
+  }
+
+  render() {
+    const experience = this.props.experience.map(exp => (
+      <tr key={exp._id}>
+        <td>{exp.company}</td>
+        <td>{exp.title}</td>
+        <td>{exp.description}</td>
+
+        <td>
+          <button
+            onClick={this.onDeleteClick.bind(this, exp._id)}
+            className="btn btn-danger"
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    ));
+    return (
+      <div>
+        <h4 className="mb-4">Payment Info</h4>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name on the Card</th>
+              <th>Card Number</th>
+              <th>Expiration Date</th>
+              <th />
+            </tr>
+            {experience}
+          </thead>
+        </table>
+      </div>
+    );
+  }
+}
+
+Experience.propTypes = {
+  deleteExperience: PropTypes.func.isRequired
+};
+
+export default connect(null, { deleteExperience })(Experience);
---
+++ client/src/components/profile/ProfileCreds.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import Moment from "react-moment";
+
+class ProfileCreds extends Component {
+  render() {
+    const { experience, education } = this.props;
+
+    const expItems = experience.map(exp => (
+      <li key={exp._id} className="list-group-item">
+        <p>
+          <strong>Name on Card:</strong> {exp.company}
+        </p>
+
+        <p>
+          <strong>Card Number:</strong> {exp.title}
+        </p>
+        <p>
+          {exp.location === "" ? null : (
+            <span>
+              <strong>cvv: </strong> {exp.location}
+            </span>
+          )}
+        </p>
+        <p>
+          {exp.description === "" ? null : (
+            <span>
+              <strong>Expiration Date: </strong> {exp.description}
+            </span>
+          )}
+        </p>
+      </li>
+    ));
+
+    const eduItems = education.map(edu => (
+      <li key={edu._id} className="list-group-item">
+        <p>
+          <strong>Contact Person:</strong> {edu.school}
+        </p>
+
+        <p>
+          <strong>Contact Number:</strong> {edu.degree}
+        </p>
+        <p>
+          <strong>Address:</strong> {edu.fieldofstudy}
+        </p>
+        <p>
+          {edu.description === "" ? null : (
+            <span>
+              <strong>Description: </strong> {edu.description}
+            </span>
+          )}
+        </p>
+      </li>
+    ));
+    return (
+      <div className="row">
+        <div className="col-md-6">
+          <br></br>
+          <h3 className="text-center text-info">PAYMENT INFO</h3>
+          {expItems.length > 0 ? (
+            <ul className="list-group">{expItems}</ul>
+          ) : (
+            <p className="text-center">No Payment Info yet</p>
+          )}
+        </div>
+
+        <div className="col-md-6">
+          <br></br>
+          <h3 className="text-center text-info">DELIVERY ADDRESS</h3>
+          {eduItems.length > 0 ? (
+            <ul className="list-group">{eduItems}</ul>
+          ) : (
+            <p className="text-center">No Delivery Address Listed</p>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProfileCreds;
